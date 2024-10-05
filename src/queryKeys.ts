9ee0031478b8afb76628c06ea89ad304b96d3066
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2024-10-05 12:02:49 +0900

    Tanstack Queryの設定

---
+++ src/queryKeys.ts
@@ -0,0 +1,6 @@
+import { createQueryKeys } from "@lukemorales/query-key-factory";
+
+export const pokemonQueryKeys = createQueryKeys("pokemon", {
+  list: () => ["list"],
+  detail: (id: number) => ["detail", id],
+});
